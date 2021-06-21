07488e67df5fda74a38123cfc672a1a520e83afb
Danny Cardoso <dcardoso066@gmail.com>
2021-06-21 17:47:35 +0200

    change number integer to string

--- db/migrate/20210621153551_add_gender_to_renters.rb
+++ db/migrate/20210621153551_add_gender_to_renters.rb
@@ -1,4 +1,5 @@
 class AddGenderToRenters < ActiveRecord::Migration[6.1]
   def change
+    add_column :renters, :gender, :string
   end
 end
--- db/migrate/20210621153758_add_genders_to_renters.rb
+++
@@ -1,5 +0,0 @@
-class AddGendersToRenters < ActiveRecord::Migration[6.1]
-  def change
-    add_column :renters, :gender, :string
-  end
-end
