29fd4432ae7a22747b4a201d58de8d7547004204
Danny Cardoso <dcardoso066@gmail.com>
2021-06-21 17:40:09 +0200

    schema done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_10_170853) do
+ActiveRecord::Schema.define(version: 2021_06_21_153758) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2021_06_10_170853) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "rental_id", null: false
+    t.string "gender"
     t.index ["rental_id"], name: "index_renters_on_rental_id"
   end
 
