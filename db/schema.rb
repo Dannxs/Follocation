4e4eae89fad39f021564062edf33be86e42efb0c
Danny Cardoso <dcardoso066@gmail.com>
2021-06-21 17:48:21 +0200

    change int to string

--- db/schema.rb
+++ db/schema.rb
@@ -36,7 +36,7 @@ ActiveRecord::Schema.define(version: 2021_06_21_153758) do
     t.string "first_name"
     t.string "last_name"
     t.string "mail"
-    t.integer "phone_number"
+    t.string "phone_number"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "rental_id", null: false
